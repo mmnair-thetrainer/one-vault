6ff943959614c66d68d9f925b0f354b59deacea3
Reshmivault <reshmiusa98@gmail.com>
2023-10-30 13:27:01 -0400

    new

--- app/src/main/java/com/example/multiple_screens_appjava/Registration.java
+++ app/src/main/java/com/example/multiple_screens_appjava/Registration.java
@@ -1,5 +1,9 @@
 package com.example.multiple_screens_appjava;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -7,20 +11,32 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class Registration extends AppCompatActivity {
 
     EditText username;
     EditText password;
-    EditText country;
     EditText dateOfBirth;
     EditText email;
     Button register;
     Button back;
+    Spinner countrySpinner;
+    ImageButton datePickerButton;
+    private int year, month, day;
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/one_vault_01";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +45,26 @@ public class Registration extends AppCompatActivity {
 
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
-        country = findViewById(R.id.country);
         dateOfBirth = findViewById(R.id.dateOfBirth);
         email = findViewById(R.id.email);
         register = findViewById(R.id.register);
         back = findViewById(R.id.back);
+        countrySpinner = findViewById(R.id.countrySpinner);
+        datePickerButton = findViewById(R.id.datePickerButton);
 
-        // Initially, disable the "Register" button
-        register.setEnabled(true);
+        datePickerButton.setOnClickListener(view -> showDatePickerDialog());
+
+        // Set up the date picker with the current date
+        Calendar calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
 
         register.setOnClickListener(view -> {
             if (checkDataEntered()) {
-                // Data is valid, navigate to the login page
-                Intent loginPageIntent = new Intent(Registration.this, MasterPasswordCreation.class);
-                startActivity(loginPageIntent);
+                // Data is valid, run the database task in a separate thread
+                new Thread(new DatabaseTask()).start();
+                // Handle UI updates here, if needed
             }
         });
 
@@ -56,9 +78,14 @@ public class Registration extends AppCompatActivity {
         // Add text change listeners to the EditText fields to enable/disable the "Register" button
         username.addTextChangedListener(textWatcher);
         password.addTextChangedListener(textWatcher);
-        country.addTextChangedListener(textWatcher);
         dateOfBirth.addTextChangedListener(textWatcher);
         email.addTextChangedListener(textWatcher);
+
+        // Populate the country spinner with data
+        String[] countries = {"USA", "IN", "UK", "Canada", "UAE"};
+        ArrayAdapter<String> countryAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, countries);
+        countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        countrySpinner.setAdapter(countryAdapter);
     }
 
     private TextWatcher textWatcher = new TextWatcher() {
@@ -75,7 +102,6 @@ public class Registration extends AppCompatActivity {
             // Check if all EditText fields are not empty and email is valid
             boolean isAllFieldsValid = !isEmpty(username) &&
                     !isEmpty(password) &&
-                    !isEmpty(country) &&
                     !isEmpty(dateOfBirth) &&
                     isEmail(email);
 
@@ -94,21 +120,28 @@ public class Registration extends AppCompatActivity {
         return TextUtils.isEmpty(str);
     }
 
+    private void showDatePickerDialog() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view, year, month, dayOfMonth) -> {
+            // Handle the selected date
+            this.year = year;
+            this.month = month;
+            this.day = dayOfMonth;
+            String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+            dateOfBirth.setText(selectedDate);
+        }, year, month, day);
+        datePickerDialog.show();
+    }
+
     boolean checkDataEntered() {
         boolean isValid = true;
 
-        if (isEmpty(username)) {
-            Toast.makeText(this, "You must enter a username to register!", Toast.LENGTH_SHORT).show();
+        if (isEmpty(username) || !isUsernameValid(username)) {
+            username.setError("Enter a valid username");
             isValid = false;
         }
 
-        if (isEmpty(password)) {
-            password.setError("Password is required!");
-            isValid = false;
-        }
-
-        if (isEmpty(country)) {
-            country.setError("Country is required!");
+        if (isEmpty(password) || !isPasswordStrong(password)) {
+            password.setError("Enter a strong password (at least 8 characters)");
             isValid = false;
         }
 
@@ -118,10 +151,64 @@ public class Registration extends AppCompatActivity {
         }
 
         if (!isEmail(email)) {
-            email.setError("Enter a valid email!");
+            email.setError("Enter a valid email address (e.g., example@gmail.com)");
             isValid = false;
         }
 
         return isValid;
+    }
+
+    boolean isUsernameValid(EditText text) {
+        String username = text.getText().toString();
+        // Add your validation logic for a valid username here (e.g., length constraints, character restrictions)
+        // For example, check if the username contains only letters and numbers and meets a minimum length requirement.
+        return username.matches("^[a-zA-Z0-9]*$") && username.length() >= 5;
+    }
+
+    boolean isPasswordStrong(EditText text) {
+        CharSequence password = text.getText().toString();
+        // Add your validation logic for a strong password here (e.g., length, complexity requirements)
+        return password.length() >= 8; // You can add more complexity checks here.
+    }
+
+    private class DatabaseTask implements Runnable {
+        @Override
+        public void run() {
+            Connection connection = null;
+
+            try {
+                // Register the JDBC driver
+                Class.forName("com.mysql.cj.jdbc.Driver"); // Use "com.mysql.cj.jdbc.Driver" for MySQL 8
+
+                // Open a connection to the database
+                connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+                // Define an SQL INSERT statement (adjust it based on your database table structure)
+                String sql = "users (username, password, date_of_birth, email) VALUES (?, ?, ?, ?)";
+
+                // Prepare the statement
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setString(1, username.getText().toString());
+                preparedStatement.setString(2, password.getText().toString());
+                preparedStatement.setString(3, dateOfBirth.getText().toString());
+                preparedStatement.setString(4, email.getText().toString());
+
+                // Execute the statement
+                preparedStatement.executeUpdate();
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                // Handle the error as needed, e.g., show an error toast
+                runOnUiThread(() -> Toast.makeText(Registration.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show());
+            } finally {
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                        // Handle the error as needed
+                    }
+                }
+            }
+        }
     }
 }
